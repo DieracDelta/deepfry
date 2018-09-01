fb78525dcc698b106c8d258c3932d326f70e24ea
Justin Restivo <jrestivo@mit.edu>
2018-09-01 12:02:02 -0400

    slight changes

--- bashwrapperboi.sh
+++ bashwrapperboi.sh
@@ -7,7 +7,8 @@ dims_b=$(identify -format " %w %h " $B_LOCATION)
 deepfry=$(python3.5 frier.py $dims_screenie $dims_screenie $dims_b $MAIM_LOCATION $B_LOCATION)
 touch $MAIM_LOCATION_2
 $($deepfry)
-convert $MAIM_LOCATION_2 -liquid-rescale 50% -fill orange -tint 100 - | convert - -liquid-rescale 200% - | convert - -modulate 300,400,300  - | xclip -selection clipboard -t image/png
+#convert $MAIM_LOCATION_2 -liquid-rescale 50% -fill orange -tint 100 - | convert - -liquid-rescale 200% - | convert - -modulate 300,400,300  - | convert - -emboss 0x1.1 - | xclip -selection clipboard -t image/png
+convert $MAIM_LOCATION_2 -liquid-rescale 50% -fill orange -tint 100 - | convert - -liquid-rescale 200% - | convert - -modulate 50,200 - | convert - -emboss 0x1.1 - | xclip -selection clipboard -t image/png
 
 
 
--- frier.py
+++ frier.py
@@ -13,16 +13,13 @@ if __name__ == "__main__":
     maim_location = "/tmp/maim_screenie.png"
     maim_location_2 = "/tmp/maim_screenie2.png"
     b_location = "bsmol.png"
-    limit = 0.98
+    limit = 0.80
     total_command = "convert -page " + str(sys.argv[1]) + "x" + str(sys.argv[2]) + "+0+0 " + maim_location + " "
     image = Image.open(maim_location)
     with PyTessBaseAPI() as api:
         api.SetImage(image)
         boxes = api.GetComponentImages(RIL.SYMBOL, True)
         for _, (__, box, _, _) in enumerate(boxes):
-            api.SetRectangle(box['x'], box['y'], box['w'], box['h'])
-            ocrResult = api.GetUTF8Text()
-            conf = api.MeanTextConf()
             if random.random() > limit: 
-                total_command += " -page " + str(sys.argv[3]) + "x" + str(sys.argv[4]) + "+" + str(box['x']) + "+" + str(box['y']) + " " + b_location + " "
+                total_command += " -page " + str(sys.argv[3]) + "x" + str(sys.argv[4]) + "+" + str(box['x']) + "+" + str(box['y'] - 6) + " " + b_location + " "
     print(total_command + " -background dodgerblue -layers flatten " + maim_location_2 + "  ")
