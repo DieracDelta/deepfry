9a900ae8fcf6c5f36e6e9cf8424c68f291e788a2
Justin Restivo <justin.p.restivo@gmail.com>
2020-03-20 22:35:25 -0400

    works with nix

--- main.sh
+++ main.sh
@@ -2,12 +2,10 @@
 MAIM_LOCATION="/tmp/maim_screenie.png"
 MAIM_LOCATION_2="/tmp/maim_screenie2.png"
 # change this line to wherever your cloned the script
-ROOT=$(pwd)
-B_LOCATION="$ROOT/bsmol.png"
 slurp | grim $MAIM_LOCATION
 dims_screenie=$(identify -format " %w %h " $MAIM_LOCATION)
 dims_b=$(identify -format " %w %h " $B_LOCATION)
-deepfry=$(python3.7 $ROOT/frier.py $dims_screenie $dims_screenie $dims_b $MAIM_LOCATION $B_LOCATION)
+deepfry=$(python3.7 $FRIER_LOCATION $dims_screenie $dims_screenie $dims_b $MAIM_LOCATION $B_LOCATION)
 touch $MAIM_LOCATION_2
 $($deepfry)
 convert $MAIM_LOCATION_2 -liquid-rescale 50% -fill orange -tint 100 - | convert - -liquid-rescale 200% - | convert - -modulate 50,200 - | convert - -emboss 0x1.1 - | wl-copy
