25121ed61d9fc24002956b9ba8ccd0074847bc28
Justin Restivo <justin.p.restivo@gmail.com>
2020-03-21 23:50:35 -0400

    rewrite in python

--- frier.py
+++ frier.py
@@ -1,23 +1,76 @@
+#!python3.7
 from PIL import Image
 from tesserocr import PyTessBaseAPI, RIL
 import random
-import sys
-
-
-
+import functools
+import subprocess
+import os
 
 
 if __name__ == "__main__":
+    b_location = os.environ.get("B_LOCATION")
+    if b_location is None or b_location == "":
+        print("error, B_LOCATION was unset")
+        exit(0)
+
     maim_location = "/tmp/maim_screenie.png"
     maim_location_2 = "/tmp/maim_screenie2.png"
-    b_location = sys.argv[8]
+    maim_location_3 = "/tmp/maim_screenie3.png"
+
+    try:
+        print("REMOVE")
+        subprocess.check_call(["rm", maim_location])
+        subprocess.check_call(["rm", maim_location_2])
+        subprocess.check_call(["rm", maim_location_3])
+    except Exception:
+        pass
+
+    print("SLURP")
+    slurp = subprocess.check_output("slurp")
+
+    print("GRIM")
+    subprocess.check_call(
+        ["grim", "-g", slurp[:-1], maim_location]
+    )
+    # subprocess.check_call(
+    # ["cp", "\"" + maim_location + "\"", "\"" + maim_location_3 + "\""]
+    # )
+
+    print("DIM CALC")
+    dims_screenshot = subprocess.check_output(
+        ["identify", "-format", "\"%w %h \"", maim_location]
+    )
+    dims_screenshot = dims_screenshot.decode("utf-8").split(" ")
+    print("DIM B CALC")
+    dims_b = subprocess.check_output(
+        ["identify", "-format", "\" %w %h \" ", b_location]
+    )
+    # subprocess.check_call("touch", main_location_2)
+    # print("dim_b" + str(dims_b))
+    # print("dim_b" + str(dims_screenshot))
+
     limit = 0.80
-    total_command = "convert -page " + str(sys.argv[1]) + "x" + str(sys.argv[2]) + "+0+0 " + maim_location + " "
+    total_command = ["convert", "-page", dims_screenshot[0]
+                     [1:]+"x"+dims_screenshot[1]+"+0+0", maim_location]
     image = Image.open(maim_location)
     with PyTessBaseAPI() as api:
         api.SetImage(image)
         boxes = api.GetComponentImages(RIL.SYMBOL, True)
         for _, (__, box, _, _) in enumerate(boxes):
             if random.random() > limit:
-                total_command += " -page " + str(sys.argv[3]) + "x" + str(sys.argv[4]) + "+" + str(box['x']) + "+" + str(box['y'] - 6) + " " + b_location + " "
-    print(total_command + " -background dodgerblue -layers flatten " + maim_location_2 + "  ")
+                total_command += ["-page", dims_screenshot[0][1:] + "x" +
+                                  dims_screenshot[1] + "+" + str(box['x']) + "+" + str(box['y'] - 6), b_location]
+    total_command += ["-background", "dodgerblue",
+                      "-layers", "flatten", maim_location_2]
+    print(functools.reduce(lambda a, b: a + " " + b, total_command))
+    print("TOUCH")
+    subprocess.check_call(["touch", maim_location_2])
+    subprocess.check_call(["touch", maim_location_3])
+    print("FINAL")
+    image.close()
+    subprocess.check_call(total_command)
+    # TODO change the wl-copy based on X vs wayland
+    subprocess.check_call(
+        ["bash", "-c", "convert", maim_location_2, "-liquid-rescale",
+         "50%", "-fill", "orange", "-tint", "100", "-", "|", "convert", "-", "-liquid-rescale", "200%", "-", "|", "convert", "-", "-modulate", "50,200", "-", "|", "convert", "-", "-emboss", "-x1.1", "-", "|", "wl-copy"]
+    )
