08ae775509127cfd8698856055929f602ace0215
Justin Restivo <justin@restivo.me>
2021-06-27 14:08:04 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "nix flake for deepfrier";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/release-21.05";
+    };
+  };
+
+  outputs = inputs:
+  let pkgs = import inputs.nixpkgs { localSystem = "x86_64-linux"; }; in
+  {
+      packages.x86_64-linux.deepfry =
+          pkgs.stdenv.mkDerivation {
+              name = "deepfrier";
+              version = 1.0;
+              src = pkgs.lib.cleanSource ./.;
+              propagatedBuildInputs = [ pkgs.xclip ];
+              meta = with pkgs.stdenv.lib; {
+                   description = "A deepfrier";
+                   homepage = "https://github.com/DieracDelta/deepfry";
+                   license = licenses.mit;
+                   platforms = platforms.linux;
+              };
+
+              nativeBuildInputs = [ pkgs.makeWrapper ];
+
+
+              installPhase = ''
+                mkdir -p $out/bin/
+                mkdir -p $out/lib/
+                cp frier.py $out/bin/
+                cp bsmol.png $out/lib/
+                chmod +x $out/bin/frier.py
+              '';
+
+
+              postFixup = ''
+                  makeWrapper $out/bin/frier.py $out/bin/deepfry --set-default B_LOCATION $out/lib/bsmol.png --set-default DISPLAY_FRONTEND X11 --prefix PATH : ${pkgs.lib.makeBinPath [ (pkgs.python37.withPackages (ps: with ps; [ pillow tesserocr ])) ] }
+              '';
+          };
+  };
+}
